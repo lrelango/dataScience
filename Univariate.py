42aad4e6d5ffbf2eff72a6363d1031fd8577bb9a
lrelango <lrelango@gmail.com>
2025-07-05 16:28:17 +0530

    Add files via upload
    Updated CentralTenancy & Univariate

--- Univariate.py
+++ Univariate.py
@@ -11,3 +11,43 @@ class Univariate():
                 #print('quan')
                 quan.append(columnName)
         return quan,qual
+    
+    def Univeriate(dataset, quan):
+        descriptive=pd.DataFrame(index=["Mean","Median","Mode","Q1:25%","Q2:50%","Q3:75%","99%","Q4:100%","IQR","1.5Rule","Lesser","Greater","Min","Max"],columns=quan)
+        for columnName in quan:
+            descriptive[columnName]["Mean"]=dataset[columnName].mean()
+            descriptive[columnName]["Median"]=dataset[columnName].median()
+            descriptive[columnName]["Mode"]=dataset[columnName].mode()[0]
+            descriptive[columnName]["Q1:25%"]=dataset.describe()[columnName]["25%"]
+            descriptive[columnName]["Q2:50%"]=dataset.describe()[columnName]["50%"]
+            descriptive[columnName]["Q3:75%"]=dataset.describe()[columnName]["75%"]
+            descriptive[columnName]["99%"]=np.percentile(dataset[columnName],99)
+            descriptive[columnName]["Q4:100%"]=dataset.describe()[columnName]["max"]
+            descriptive[columnName]["IQR"]=descriptive[columnName]["Q3:75%"]-descriptive[columnName]["Q1:25%"]
+            descriptive[columnName]["1.5Rule"]=1.5*descriptive[columnName]["IQR"]
+            descriptive[columnName]["Lesser"]=descriptive[columnName]["Q1:25%"]-descriptive[columnName]["1.5Rule"]
+            descriptive[columnName]["Greater"]=descriptive[columnName]["Q4:100%"]+ descriptive[columnName]["1.5Rule"]
+            descriptive[columnName]["Min"]=dataset[columnName].min()
+            descriptive[columnName]["Max"]=dataset[columnName].max()
+        return descriptive
+    
+    def freqTable(columnName,dataset):
+        freqTable=pd.DataFrame(columns=["Unique_values","Frequency","Relative Frequency","CumSum"])
+        freqTable["Unique_values"]=dataset[columnName].value_counts().index
+        freqTable["Frequency"]=dataset[columnName].value_counts().values
+        freqTable["Relative Frequency"]=freqTable["Frequency"]/103
+        freqTable["CumSum"]=freqTable["Relative Frequency"].cumsum()
+        return freqTable
+    
+    def cleaning_outliers(dataset, descriptive,quan):
+        lesser=[]
+        greater=[]
+        for columnName in quan:
+            if(descriptive[columnName]["Min"]<descriptive[columnName]["Lesser"]):
+                lesser.append(columnName)
+            if(descriptive[columnName]["Max"]>descriptive[columnName]["Greater"]):
+                greater.append(columnName)
+        for columnName in lesser:
+            dataset[columnName][dataset[columnName]<descriptive[columnName]["Lesser"]]=descriptive[columnName]["Lesser"]
+        for columnName in greater:
+            dataset[columnName][dataset[columnName]>descriptive[columnName]["Greater"]]=descriptive[columnName]["Greater"]
